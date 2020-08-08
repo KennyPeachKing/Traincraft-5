d59ae6d606c786a2451fc7abd853685c33141f95
JCRail <terrillbrayden@gmail.com>
2020-08-07 19:52:24 -0500

    Volume Change on Leslie RS3k
    It was too quiet. Now it sounds louder. Yay

--- src/main/java/train/common/library/EnumSounds.java
+++ src/main/java/train/common/library/EnumSounds.java
@@ -96,7 +96,7 @@ public enum EnumSounds {
 	U23B(com.jcirmodelsquad.tcjcir.locomotives.DieselU23B.class, "leslie_s3l", 2.5F, "7fdl-12_notch8", 0.65F, 40, "7fdl-12_idle", 0.65F, 50, true),
 	SW1500(com.jcirmodelsquad.tcjcir.locomotives.DieselSW1500.class, "nathan_p3", 0.65F, "16-645e_notch8", 0.65F, 10, "16-645e_idle", 0.8F, 3, true),
 	SD45dash2(com.jcirmodelsquad.tcjcir.locomotives.DieselSD45dash2.class, "leslie_s3l", 2.5F, "645e3_notch8", 0.65F, 10, "645e3_idle", 0.65F, 3, true),
-	SD40dash2(com.jcirmodelsquad.tcjcir.locomotives.DieselSD40dash2.class, "leslie_rs3k", 0.65F, "16-645e_notch8", 0.65F, 10, "16-645e_idle", 0.8F, 5, true),
+	SD40dash2(com.jcirmodelsquad.tcjcir.locomotives.DieselSD40dash2.class, "leslie_rs3k", 2.5F, "16-645e_notch8", 0.65F, 10, "16-645e_idle", 0.8F, 5, true),
 	CF7(com.jcirmodelsquad.tcjcir.locomotives.DieselCF7.class, "nathan_k3la", 0.65F, "567_notch8", 0.65F, 10, "567_idle", 0.8F, 3, true),
 	SD9(com.jcirmodelsquad.tcjcir.locomotives.DieselSD9.class, "nathan_k3la", 2.5F, "567_notch8", 0.45F, 10, "567_idle", 0.75F, 3, true),
 	SF30C(com.jcirmodelsquad.tcjcir.locomotives.DieselSF30C.class, "leslie_s3", 2F, "fdl16_notch8", 0.5F, 10, "fdl16_idle", 0.5F, 3, true),
